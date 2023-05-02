266fb22197de73a8d4ef81fdc005e555ebbf36e7
SpacemenDevSquad <spaceman228409@gmail.com>
2023-05-01 23:39:22 -0400

    Website UI Update
    Ooooooooo stars

---
+++ js/clickAudio.js
@@ -0,0 +1,9 @@
+var audio = document.getElementById("click");
+var clickSound = new Audio()
+clickSound.src = "SFX/click.mp3"
+
+$(".inactive").mouseenter(function() {
+    var clickSound = new Audio()
+    clickSound.src = "SFX/click.mp3"
+    clickSound.play();
+});
